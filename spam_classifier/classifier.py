b4efcbb33cd7c64cfb45ffab417685684fc85cb4
Parley Martins <parleypachecomartins@gmail.com>
2016-04-01 13:04:58 -0600

    First version to read the file

--- spam_classifier/classifier.py
+++ spam_classifier/classifier.py
@@ -1,2 +1,12 @@
-def read_file():
-	pass
+words = {}
+def read_file(file_address = 'teste'):
+	with open(file_address, encoding='utf-8') as f:
+		line = f.read()
+		for word in line.split():
+			try:
+				words[word] += 1
+			except:
+				words[word] = 1
+
+read_file()
+print(words)
